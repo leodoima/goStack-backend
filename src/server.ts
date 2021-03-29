f25392007cb11ad88f3cff293ae48dda5e2eaaca
Leonardo dos Santos <leodoima@hotmail.com>
2021-03-29 20:50:47 -0300

    Criando primeiro agendamento

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Rota acessada com sucesso!' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🍧 Server on Fire!');

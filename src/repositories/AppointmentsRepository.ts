3b8a6bb79cb309a5f21b599e085963152afa073d
Leonardo dos Santos <leodoima@hotmail.com>
2021-04-04 18:17:19 -0300

    Ajuste de repositorio Typeorm e integraçao de banco com classes da aplicaçao

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -4,11 +4,11 @@ import { EntityRepository, Repository } from 'typeorm';
 import Appointment from '../models/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository {
-  public findByDate(date: Date): Appointment | null {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: { date },
+    });
 
     return findAppointment || null;
   }

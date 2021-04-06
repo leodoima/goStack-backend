371accc863455949ec158d34fac6dc907f73484b
Leonardo dos Santos <leodoima@hotmail.com>
2021-04-05 22:10:06 -0300

    Criando relacionamento entre tabelas

--- src/database/migrations/1617233674216-CreateAppointments.ts
+++ src/database/migrations/1617233674216-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1617233674216
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
